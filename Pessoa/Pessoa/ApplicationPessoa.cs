eab915168cfd9c67e5025fb0955b31d96217ef19
Evandrolds <evandro.lds.ds@outlook.com>
2022-11-24 17:17:32 -0300

    Trabalhando POO com a classe Pessoa

---
+++ Pessoa/Pessoa/ApplicationPessoa.cs
@@ -0,0 +1,37 @@
+﻿using dados_de_pessoa;
+using System;
+using System.Globalization;
+
+namespace ApplicationPessoa {
+
+    class Application {
+        static void Main(string[] args) {
+
+            Pessoa p1 = new Pessoa("Sandro", 36);
+            Console.WriteLine(p1.ToString());
+
+            Pessoa p2;
+
+            p2 = new Pessoa("Evandro", 23);
+
+            Console.WriteLine(p2.ToString());
+
+            // Verificar quem é mais velho
+            Console.WriteLine("O mais velho dentre as pessoas é " + VerificarMaior(p1, p2));
+
+        }
+        // Metodo de verificação de idade
+        public static string VerificarMaior(Pessoa pessoa1, Pessoa pessoa2) {
+
+
+            if (pessoa1.GetIdade() > pessoa2.GetIdade()) {
+                return pessoa1.GetNome();
+            }
+            else {
+                return pessoa2.GetNome();
+            }
+
+        }
+
+    }
+}
---
+++ Pessoa/Pessoa/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿namespace dados_de_pessoa;
+
+class Pessoa {
+
+    public Pessoa() { }
+    public Pessoa(string nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    private string nome;
+    private int idade;
+
+    public string GetNome() {
+        return nome;
+    }
+    public int GetIdade() {
+        return idade;
+    }
+    public void SetNome(String nome) {
+        this.nome = nome;
+    }
+    public void SetIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public override string? ToString() {
+        return "Nome: " + nome + "\nIdade: " + idade;
+    }
+   
+}
