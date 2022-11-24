60cad4373fb7c1703739d1650065b7574d1ff04a
Evandrolds <evandro.lds.ds@outlook.com>
2022-11-23 22:32:52 -0300

    Calculo do triangulo com POO

---
+++ PrimeirProjetoPOO/PrimeirProjetoPOO/Program.cs
@@ -0,0 +1,26 @@
+﻿using PrimeirProjetoPOO;
+using System;
+using System.Globalization;
+
+namespace CalcularAreaDoTriangulo_X_Y;
+class Application {
+    static void Main(string[] args) {
+        Triangulo x, y,area;
+        area= new Triangulo();
+        x = new Triangulo();
+        y = new Triangulo();
+
+        Console.WriteLine("Entre com os valores de X");
+        x.A = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        x.B = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        x.C = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+        Console.WriteLine(  "Area X " + area.Somar(x).ToString("F4", CultureInfo.InvariantCulture));
+
+        Console.WriteLine("Entre com os valores de Y");
+        y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine(  "Area X " + area.Somar(y).ToString("F4",CultureInfo.InvariantCulture));
+    }
+}
---
+++ PrimeirProjetoPOO/PrimeirProjetoPOO/Triangulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimeirProjetoPOO {
+     class Triangulo {
+
+        public double A;
+        public double B;
+        public double C;
+
+        public double Somar(Triangulo t) {
+            double p = (t.A + t.B + t.C) /2;
+
+            double area = Math.Sqrt(p * (p - t.A) * (p - t.B) * (p - t.C));
+            return area;
+        }
+    }
+}
