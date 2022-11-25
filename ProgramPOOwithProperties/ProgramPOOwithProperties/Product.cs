aa6c7688e575aa1c120ed34081d93dd198c6eefc
Evandrolds <evandro.lds.ds@outlook.com>
2022-11-25 10:07:05 -0300

    Update Name attribute

--- ProgramPOOwithProperties/ProgramPOOwithProperties/Product.cs
+++ ProgramPOOwithProperties/ProgramPOOwithProperties/Product.cs
@@ -2,19 +2,27 @@
 namespace ProgramWithProperties;
 
 class Product {
-    public string Name { get; set; }
+    private string _name;
     public string Description { get; set; }
     public int Quantity { get; private set; }
     public double Price { get; private set; }
 
     public Product(string name, string description, int quantity, double price) {
-       Name = name;
+        this._name = name;
         Description = description;
         Quantity = quantity;
         Price = price;
     }
-    
-   
+    public string GetName() {
+        return _name;
+    }
+    public void SetName(string name) {
+        if (_name != null && _name.Length > 1) {
+            _name = name;
+
+        }
+    }
+
     public void AddProduct(int quantity) {
         if (quantity > 0) {
             Quantity = quantity;
@@ -31,15 +39,15 @@ class Product {
         return 0;
     }
     public double TotalValueInventoies() {
-        return Price* Quantity;
+        return Price * Quantity;
     }
     override
     public string ToString() {
-     return " Name: " + Name +
-            "\n Description: " +Description +
-            "\n Quantity: " + Quantity +
-            "\n Price R$ " + Price.ToString("F2",CultureInfo.InvariantCulture) +
-            "\n Total value inventoies R$ " + TotalValueInventoies().ToString("F2", CultureInfo.InvariantCulture);
+        return " Name: " + _name +
+               "\n Description: " + Description +
+               "\n Quantity: " + Quantity +
+               "\n Price R$ " + Price.ToString("F2", CultureInfo.InvariantCulture) +
+               "\n Total value inventoies R$ " + TotalValueInventoies().ToString("F2", CultureInfo.InvariantCulture);
     }
 
 }
