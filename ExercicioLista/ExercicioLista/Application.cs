477c09ec474bf2fc04de42442276eaa9069332d6
Evandrolds <evandro.lds.ds@outlook.com>
2022-11-27 21:33:04 -0300

    Exercicio Employee

---
+++ ExercicioLista/ExercicioLista/Application.cs
@@ -0,0 +1,51 @@
+﻿using employee;
+using System.Globalization;
+
+namespace ExercicioLista {
+    class Application {
+        static void Main(string[] args) {
+
+            Console.WriteLine("How many employee do you want register?");
+            List<Employee> employeeList = new List<Employee>();
+            int n = int.Parse(Console.ReadLine());
+            Employee employee = new Employee();
+            for (int i = 1; i <= n; i++) {
+
+                Console.Write("Enter " + i + "º Id of employee...");
+                int id2 = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter name ...");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter salary $ ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.WriteLine("\n");
+
+                employee = new Employee(id2, name, salary);
+                employeeList.Add(employee);
+
+                Console.WriteLine(employee);
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("\n");
+            Console.Write("Enter employee id that will have increase salary ...");
+            int id = int.Parse(Console.ReadLine());
+            Employee emp = employeeList.Find(x => x.Id == id);
+            if (emp != null) {
+
+                Console.WriteLine("Enter increase value $ ");
+                double increase = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.Increase(increase);
+            }
+            else {
+                Console.WriteLine(" Invalid id ");
+            }
+            Console.WriteLine("\n");
+
+            employeeList.ForEach(x => Console.WriteLine(x));
+
+        }
+
+    }
+}
---
+++ ExercicioLista/ExercicioLista/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace employee;
+
+class Employee {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Salary { get; private set; }
+    public Employee() { }
+
+    public Employee(int id, string name, double salary) {
+        Id = id;
+        Name = name;
+        this.Salary = salary;
+    }
+    override
+    public string ToString() {
+        return " Id: " + Id +
+            "\n Name: " + Name +
+            "\n Salary $ " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+    }
+    public void Increase(double increase) {
+        Salary += (Salary * increase) / 100;
+    }
+}
