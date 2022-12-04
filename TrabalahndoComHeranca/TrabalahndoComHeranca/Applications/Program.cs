a191af39c44b1023fb7d34d1126cfd1732e01461
Evandrolds <evandro.lds.ds@outlook.com>
2022-12-04 11:37:01 -0300

    Exemplo com Heranca

---
+++ TrabalahndoComHeranca/TrabalahndoComHeranca/Applications/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using TrabalahndoComHeranca.Entities;
+
+namespace TrabalahndoComHeranca.Applications
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Account c1 = new Account("Sandro", 12345, 520.0);
+            BusinessAccount b1 = new BusinessAccount("Engenharia", 12451, 20000.0, 2500.0);
+            Console.WriteLine(c1);
+            Console.WriteLine(b1);
+
+            double loan = 3000.0;
+            
+            Console.WriteLine("\n Loan amount $ " + loan);
+            b1.Loan(loan);
+            
+            Console.WriteLine(b1);
+            
+            c1.Deposit(480.0);
+            Console.WriteLine(c1);
+        }
+    }
+}
