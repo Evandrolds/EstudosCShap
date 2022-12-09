f47e3151498bd24abd510879034ed6c866315e4f
Evandrolds <evandro.lds.ds@outlook.com>
2022-12-08 21:18:50 -0300

    updates folders

--- TrabalahndoComHeranca/TrabalhandoComHeranca/Applications/Program.cs
+++ TrabalahndoComHeranca/TrabalhandoComHeranca/Applications/Program.cs
@@ -9,6 +9,7 @@ namespace TrabalahndoComHeranca.Applications
         {
             Account c1 = new Account("Sandro", 12345, 520.0);
             BusinessAccount b1 = new BusinessAccount("Engenharia", 12451, 20000.0, 2500.0);
+           
             Console.WriteLine(c1);
             Console.WriteLine(b1);
 
@@ -21,6 +22,22 @@ namespace TrabalahndoComHeranca.Applications
             
             c1.Deposit(480.0);
             Console.WriteLine(c1);
+
+            // Upcasting
+            Account cc1 = b1;
+            Console.WriteLine(cc1);
+
+            Account cc2 = new BusinessAccount();
+            Account cc3 = new SavingsAccount();
+
+
+            // Downcasting
+            BusinessAccount cc4 = (BusinessAccount) cc2;
+
+            // outra forma de fazer downcast
+            BusinessAccount cc5 = cc2 as BusinessAccount;
+            cc4.Loan(54.0);
+            Console.WriteLine(cc4);
         }
     }
 }
