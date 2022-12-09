3b322b2522c052292f8099d45fd6b3c03fcf7dfb
Evandrolds <evandro.lds.ds@outlook.com>
2022-12-08 21:14:14 -0300

    updates end new BusinessAccount entity

---
+++ TrabalahndoComHeranca/TrabalhandoComHeranca/Entities/SavingsAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalahndoComHeranca.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double interestRate { get; set; }
+
+        public SavingsAccount(string holder, int number, double balance, double interestRate) 
+            : base(holder, number, balance)
+        {
+            this.interestRate = interestRate;
+        }
+        public void UpdateBalance()
+        {
+            Balance += Balance * interestRate;
+        }
+
+        public SavingsAccount()
+        {
+        }
+    }
+}
